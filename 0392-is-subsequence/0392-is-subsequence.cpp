036b803a69acb2334a325862d145e012888af024
HsuanYouLin <yoyomy7198come@gmail.com>
2024-02-18 14:22:07 -0600

    add 0392-is-subsequence c/c++ solutions

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,23 @@
+bool isSubsequence(char* s, char* t) {
+    // special case, if s is empty, return true, because empty could be any subsequence of t
+    if (strlen(s) == 0){
+        return true;
+    }
+    
+    // initialize index j to track current position in string s
+    int j = 0; // if forget to initialize, it may hold any garbage value
+
+    // iterate through string t
+    for (int i = 0; i < strlen(t); i++){
+        // if index s[j] == t[i], increment index j to the next position of string s
+        if (s[j] == t[i]){
+            j++;
+        }
+        // if index j reach the end of the string s, means find all the match, return true
+        if (j == strlen(s)){
+            return true;
+        }
+    }
+    // if reach the end of the string s, still not find all characters, return false
+    return false;
+}
