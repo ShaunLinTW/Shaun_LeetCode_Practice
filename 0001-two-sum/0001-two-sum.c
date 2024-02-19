aa852c3508d473681cc055cad852f1bfe557bbad
HsuanYouLin <yoyomy7198come@gmail.com>
2024-02-18 20:06:54 -0600

    update 001-two-sum c solution annotation, and initialize 0583-delete-operation-for-two-strings questions

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -2,7 +2,7 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    *returnSize = 2; // 2 elements
+    *returnSize = 2; // initialize the returnSize pointer to 2, which is the size of the returned array
     int* result = (int*)malloc(sizeof(int) * 2); // allocate memory for 2 elements
     // iterate through the array's 1st element
     for (int i = 0; i < numsSize; i++) {
