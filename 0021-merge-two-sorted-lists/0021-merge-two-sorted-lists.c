1fdba00787e8e324d4c4f20912e582261f9156ce
HsuanYouLin <yoyomy7198come@gmail.com>
2024-02-26 13:53:50 -0600

    update 0021-merge-two-sorted-lists C solution & add 0023-merge-k-sorted-lists problem

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -6,26 +6,35 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
-    // Create a dumy node to store the merged list
+    // create the dumy node to store the merged list, and curr node for dynamic storing
     struct ListNode *dumy = (struct ListNode *)malloc(sizeof(struct ListNode));
-    struct ListNode *cur = dumy; // Create a pointer to the dumy node, so the dumy->next will be the head of the merged list
+    struct ListNode *curr = dumy;
 
-    // While both lists are not empty
-    while(list1 && list2) {
-        if(list1->val < list2->val) {
-            cur->next = list1; // add list1 to current list
-            list1 = list1->next; // move list1 to the next node
-        } else {
-            cur->next = list2;
+    // traverse through two list, if not empty, merge them into curr node
+    while (list1 && list2){
+        // if list1 val bigger then list2, make curr next node equal to list2
+        if (list1->val > list2->val){
+            curr->next = list2;
+            // move list2 to next node
             list2 = list2->next;
         }
-        cur = cur->next;
+        // if list1 val smaller then list2, make curr next node equal to list1
+        else {
+            curr->next = list1;
+            // move list1 to next node
+            list1 = list1->next;
+        }
+        // move curr to next node
+        curr = curr->next;
     }
     // append the rest of the list to the merged list
-    if(list1) {
-        cur->next = list1;
-    } else {
-        cur->next = list2;
+    if (list1 != NULL){
+        curr->next = list1;
     }
-    return dumy->next; // return the next node of dumy, which is the head of the merged list
+    else {
+        curr->next = list2;
+    }
+    
+    // return next node of dumy node, which is the head of merged linked list
+    return dumy->next;
 }
