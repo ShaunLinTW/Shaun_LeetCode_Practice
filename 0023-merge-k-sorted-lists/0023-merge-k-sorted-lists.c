8a7e512dc9036a3d8bb156a3c3e54cfca67d1cde
HsuanYouLin <yoyomy7198come@gmail.com>
2024-03-04 00:55:52 -0600

    update 0023-merge-k-sorted-lists c++ solution

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.c
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.c
@@ -5,6 +5,12 @@
  *     struct ListNode *next;
  * };
  */
+
 struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+    // create the dumy node to store the merged list, and curr node for dynamic sotring
+    struct ListNode *dumy = (struct ListNode *)malloc(sizeof(struct ListNode));
+    struct ListNode *curr = dumy;
+
+    // traverse through two list, if not empty, merge them into curr node
     
 }
