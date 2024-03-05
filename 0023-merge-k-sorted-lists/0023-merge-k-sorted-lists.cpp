0d09b48885d1a591cef2108d9226b66612d57123
HsuanYouLin <yoyomy7198come@gmail.com>
2024-03-05 13:39:44 -0600

    update 0023-merge-k-sorted-lists C++ solution

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -9,61 +9,53 @@
  * };
  */
 class Solution {
-public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int n = lists.size();
-        if (n == 0) {
-            return NULL;
-        }
-        
-        while (n > 1) {
-            for (int i = 0; i < n / 2; i++) {
-                lists[i] = mergeTwoLists(lists[i], lists[n - i - 1]);
-            }
-            n = (n + 1) / 2;
-        }
-        
-        return lists.front();
-    }
 private:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1 == NULL && list2 == NULL) {
-            return NULL;
-        }
-        if (list1 == NULL) {
-            return list2;
-        }
-        if (list2 == NULL) {
-            return list1;
-        }
-        
-        ListNode* head = NULL;
-        if (list1->val <= list2->val) {
-            head = list1;
-            list1 = list1->next;
-        } else {
-            head = list2;
-            list2 = list2->next;
-        }
-        ListNode* curr = head;
-        
-        while (list1 != NULL && list2 != NULL) {
-            if (list1->val <= list2->val) {
-                curr->next = list1;
-                list1 = list1->next;
-            } else {
+    // helper function to merge two lists
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+        // Create a dumy node to store the merged list, and set the current pointer to the dumy node
+        ListNode *dumy = new ListNode(0);
+        ListNode *curr = dumy;
+
+        // while both lists are not empty
+        while (list1 != NULL && list2 != NULL){
+            if (list1->val > list2->val){
                 curr->next = list2;
                 list2 = list2->next;
             }
+            else{
+                curr->next = list1;
+                list1 = list1->next;
+            }
             curr = curr->next;
         }
-        
-        if (list1 == NULL) {
-            curr->next = list2;
-        } else {
+
+        // Append the remaining nodes in either list to the merged list
+        if (list1 != NULL){
             curr->next = list1;
         }
-        
-        return head;
+        else {
+            curr->next = list2;
+        }
+        return dumy->next;
+    }
+
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // If the list is empty, return NULL
+        if (lists.size() == 0){
+            return NULL;
+        }
+        // if the list has only one element, return the list
+        else if (lists.size() == 1){
+            return lists[0];
+        }
+
+        // if the list has more than one element, merge the lists using the mergeTwoLists function
+        else {
+            for (int i = 1; i < lists.size(); i++){
+                lists[i] = mergeTwoLists(lists[i-1], lists[i]);
+            }
+        }
+        return lists[lists.size()-1];
     }
 };
