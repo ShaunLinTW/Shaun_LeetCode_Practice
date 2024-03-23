31a1d0cda4ac1d307d28fa19e7d24d657c3269e2
HsuanYouLin <yoyomy7198come@gmail.com>
2024-03-23 15:52:29 -0500

    update 0237-delete-node-in-a-linked-list C&C++ solutions (avoid memory leak issues)

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.c
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.c
@@ -6,6 +6,22 @@
  * };
  */
 void deleteNode(struct ListNode* node) {
-    node->val = node->next->val;
-    node->next = node->next->next;
+        // simplist way to delete a node is to copy the value of the next node to the current node and delete the next node
+        // node->val = node->next->val;
+        // node->next = node->next->next;
+
+        // avoid memory leak:
+        // corner case
+        if (node->next == NULL){
+            free(node);
+            return;
+        }
+        // copy next val to current val
+        node->val = node->next->val;
+        // use a temp node to store next next node
+        struct ListNode* temp = node->next->next;
+        // free next node to avoid memory leak issues
+        free (node->next);
+        // copy temp after current next node
+        node->next = temp;
 }
--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,7 +9,18 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
+        // simplist way to delete a node is to copy the value of the next node to the current node and delete the next node
+        // node->val = node->next->val;
+        // node->next = node->next->next;
+
+        // avoid memory leak:
+        if (node->next == NULL) {
+            delete (node);
+            return;
+        }
         node->val = node->next->val;
-        node->next = node->next->next;
+        ListNode *temp = node->next->next;
+        delete (node->next);
+        node->next = temp;
     }
 };
