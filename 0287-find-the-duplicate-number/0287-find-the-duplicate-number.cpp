8c51bce6de0d15b5287bacf4667a81f000daadb8
HsuanYouLin <yoyomy7198come@gmail.com>
2024-03-29 01:56:10 -0500

    update 0287-find-the-duplicate-number C++ solution (interview shouldn't test this, too hard...)

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        
+        int left = 1, right = nums.size() - 1;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            
+            // count the number of elements smaller/ equal than middle element
+            int c = 0;
+            for(const int& el: nums)
+                if(el <= mid)
+                    ++c;
+            
+            if(c > mid)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
     }
 };
