47d79c77d17714feb25dba9275b32d30dc95c100
HsuanYouLin <yoyomy7198come@gmail.com>
2024-03-30 17:24:50 -0500

    update 0026-remove-duplicates-from-sorted-array C&C++ solutions

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -1,3 +1,14 @@
 int removeDuplicates(int* nums, int numsSize) {
-    
+    // initialize index of visited array
+    int index = 1;
+
+    // don't check zero index, start from first index
+    for (int i = 1; i < numsSize; i++){
+        // add value to visited array, if it's not equal to visited value
+        if (nums[i] != nums[i - 1]){
+            nums[index] = nums[i];
+            index++; //increment index
+        }
+    }
+    return index;
 }
--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+        // initialize a index for the visited array
+        int index = 1;
+
+        // don't check zero index's value, start from first index
+        for (int i = 1; i < nums.size(); i++){
+            // if current array's value not equals to visited array's value, add that value to visited array, and increment index of the visited array
+            if (nums[i] != nums[i - 1]){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
     }
 };
