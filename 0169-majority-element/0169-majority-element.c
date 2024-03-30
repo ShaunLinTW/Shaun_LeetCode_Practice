2704dd0ec66297262c1199209422f1773307af90
HsuanYouLin <yoyomy7198come@gmail.com>
2024-03-30 18:29:48 -0500

    update 0169-majority-element C&C++ solutions

--- 0169-majority-element/0169-majority-element.c
+++ 0169-majority-element/0169-majority-element.c
@@ -1,3 +1,25 @@
 int majorityElement(int* nums, int numsSize) {
-    
+    // in C language, default library don't have unordered_map or sor algorithm
+    // Moore Volting Algorithm approach
+    // initialize count and candidate
+    int count = 0;
+    int candidate = 0;
+
+    // iterate through the array
+    for (int i = 0; i < numsSize; i++){
+        // if count is 0, which means all candidates are same count or not start volting yet,
+        // therefore, assign the current element as new candidate and increment by 1
+        if (count == 0) {
+            candidate = nums[i];
+            count++;
+        }
+        // if current element same as candidate, which means it got one volt, then increment count by 1
+        else if(nums[i] == candidate){
+            count++;
+        }
+        else{
+            count--;
+        }
+    }
+    return candidate; // return most voting candidate, which is the Majority Element, this could be work for not even n/2 case
 }
--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        // 1st way (Sorting):
+        // sort the array in non-decreasing order
+        sort(nums.begin(), nums.end());
+        // initialize the size of the array
+        int n = nums.size();
+        // return n/2, because the majority element will always present at index n/2
+        return nums[n/2];
+
+        // 2nd way (Hash Map):
+        // initialize the size of the array, and using unordered_map to create hash map
+        // int n = nums.size();
+        // unordered_map<int, int> map;
         
+        // // iterate through array, and increments its count in the hash map
+        // for(int i = 0; i < n; i++){
+        //     // one line code:
+        //     // map[nums[i]]++; // access or insert the value into hash map & increment its count
+            
+        //     // multiple line code:
+        //     int key = nums[i]; // extract key
+        //     int& value = map[key]; // access or insert the value associated with the key in the map
+        //     value++; // increment value
+        // }
+
+        // n = n/2; // get the middle of the array
+        // // iterate through hash map
+        // for(auto x: map){
+        //     // if the count greater than n, return it's key, which is the majority element.
+        //     if(x.second > n){
+        //         return x.first;
+        //     }
+        // }
+        // return 0;
     }
 };
