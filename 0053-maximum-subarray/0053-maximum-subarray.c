7b8ef7b2685c681a3b6538d5902081c995a30a10
HsuanYouLin <yoyomy7198come@gmail.com>
2024-03-31 16:42:36 -0500

    update 0053-maximum-subarray C&C++ solutions

--- 0053-maximum-subarray/0053-maximum-subarray.c
+++ 0053-maximum-subarray/0053-maximum-subarray.c
@@ -1,3 +1,20 @@
 int maxSubArray(int* nums, int numsSize) {
-    
+    // initialize masSum and currentSum 
+    int maxSum = INT_MIN;
+    int currentSum = 0;
+
+    for (int i = 0; i < numsSize; i++){
+        // update currentSum
+        currentSum += nums[i];
+
+        // if currentSum greater than maxSum, update maxSum to it
+        if (currentSum > maxSum){
+            maxSum = currentSum;
+        }
+        // if currentSum smaller than 0, reset it to 0
+        if (currentSum < 0){
+            currentSum = 0;
+        }
+    }
+    return maxSum;
 }
--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        
+        // <Simple solution>:
+        // initialize max sum, use INT_MIN to ensure that any valid subarray sum will be greater than it
+        int maxSum = INT_MIN;
+        // initialize current sum
+        int currentSum = 0;
+
+        // iterate through array
+        for (int i = 0; i < nums.size(); i++){
+            currentSum += nums[i];
+
+            // if currentSum greater than maxSum, update maxSum to it
+            if (currentSum > maxSum){
+                maxSum = currentSum;
+            }
+            // if smaller than 0, reset currentSum to 0
+            if (currentSum < 0){
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+
+        // <used max function>:
+        // int maxSum = INT_MIN;
+        // int currentSum = 0;
+
+        // for (int i = 0; i < nums.size(); i++){
+        //     currentSum = max(currentSum + nums[i], nums[i]);
+        //     maxSum = max(maxSum, currentSum);
+        // }
+        // return maxSum;
     }
 };
