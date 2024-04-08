0a66fcef4021c73de05a76877a2dd30a808c2230
HsuanYouLin <yoyomy7198come@gmail.com>
2024-04-07 19:10:24 -0500

    update 0167-two-sum-ii-input-array-is-sorted C&C++ solutions

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.c
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.c
@@ -2,5 +2,34 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* numbers, int numbersSize, int target, int* returnSize) {
-    
+    // set return size to 2
+    *returnSize = 2;
+    // initialize result array using pointer
+    int* result = (int*)malloc(sizeof(int) * (*returnSize));
+    // initialize two pointers
+    int j = 0;
+    int k = numbersSize - 1;
+
+    // iterate through array until index j meet k
+    while (j < k){
+        // calculate sum of the two indices
+        int sum = numbers[j] + numbers[k];
+
+        // if sum equal to target, update result's array
+        if (sum == target){
+            // note that: return index start from 1 & C array don't have append operation, so just start from result array' index 0
+            result[0] = j + 1;
+            result[1] = k + 1;
+            return result;
+        }
+        // if sum greater then target, move index k to left
+        else if (sum > target){
+            k--;
+        }
+        // if sum less then target, move index j to right
+        else {
+            j++;
+        }
+    }
+    return result;
 }
--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        
+        // initialize result arrauy
+        vector<int> result;
+        // initialize two pointers from the left side of the array and right side of the array
+        int j = 0;
+        int k = numbers.size() - 1;
+
+        // iterate through array until left index j meet right index k
+        while (j < k){
+            // calculate sum of the two indices' number
+            int sum = numbers[j] + numbers[k];
+            
+            // if sum equal target, append indices to result array
+            if (sum == target){
+                // note that: return index start from 1
+                result.push_back(j+1);
+                result.push_back(k+1);
+                break; // break the while loop
+            }
+            // if sum greater then target, move right index k to left
+            else if (sum > target){
+                k--;
+            }
+            // if sum less then target, move left index j to right
+            else{
+                j++;
+            }
+        }
+        return result;
     }
 };
