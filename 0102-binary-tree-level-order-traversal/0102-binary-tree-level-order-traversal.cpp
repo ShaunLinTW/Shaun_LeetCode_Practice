5ce8911ecd5b39c7509d5d806564aae6cbea1ec6
HsuanYouLin <yoyomy7198come@gmail.com>
2024-04-10 19:37:51 -0500

    update 0102-binary-tree-level-order-traversal C++ solutiopn

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,6 +12,40 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        
+        // empty case
+        if (root == NULL){
+            return {};
+        }
+        // initialize results vector
+        vector<vector<int>> results;
+        // initialize queue as a vector of TreeNode pointers
+        queue<TreeNode*> q;
+        q.push(root);
+
+        // create a temorary TreeNode pointer
+        TreeNode * temp_tree;
+        // initialize length of the queue
+        int length;
+
+        // iterate through queue until queue empty
+        while(!q.empty()){
+            // initialize vector and queue's length
+            length = q.size();
+            vector<int> v;
+            // iterate through queue using breadth-first search
+            for (int i = 0; i < length; i++){
+                temp_tree = q.front(); // get front of queue to temporary TreeNode
+                q.pop(); // pop front of queue
+                // add temporary TreeNode's value to vector
+                v.push_back(temp_tree->val);
+
+                // if tree's left child have value, push it to queue
+                if (temp_tree->left != NULL) q.push(temp_tree->left);
+                // if tree's right child have value, push it to queue
+                if (temp_tree->right != NULL) q.push(temp_tree->right);
+            }
+            results.push_back(v);
+        }
+        return results;
     }
 };
