da4eddf098ffd77cf684233910a999de1c25a1e2
HsuanYouLin <yoyomy7198come@gmail.com>
2024-04-10 23:04:51 -0500

    update 0226-invert-binary-tree C solution (DFS)

--- 0226-invert-binary-tree/0226-invert-binary-tree.c
+++ 0226-invert-binary-tree/0226-invert-binary-tree.c
@@ -7,5 +7,16 @@
  * };
  */
 struct TreeNode* invertTree(struct TreeNode* root) {
+    // edge case
+    if (root == NULL) return NULL;
+    // DFS
+    invertTree(root->left);
+    invertTree(root->right);
+
+    // create a temp treenode and swap left&right tree
+    struct TreeNode* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
     
+    return root;
 }
