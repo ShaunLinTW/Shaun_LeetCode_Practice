08041e9f08d4342fa4b22a25c28479b28429bdf1
HsuanYouLin <yoyomy7198come@gmail.com>
2024-04-16 18:54:02 -0500

    update 0098-validate-binary-search-tree C solution

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.c
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.c
@@ -6,6 +6,14 @@
  *     struct TreeNode *right;
  * };
  */
+
+bool isValid(struct TreeNode* root, struct TreeNode* max, struct TreeNode* min){
+    if (root == NULL) return true;
+    if ((min == NULL || root->val > min->val) && (max == NULL || root->val < max->val))
+        return isValid(root->left, root, min) && isValid(root->right, max, root);
+    return false;
+}
+
 bool isValidBST(struct TreeNode* root) {
-    
+    return isValid(root, NULL, NULL);
 }
