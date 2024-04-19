e95c18382f160ade5e4714561849a6e4bd38a9e1
HsuanYouLin <yoyomy7198come@gmail.com>
2024-04-19 18:25:12 -0500

    update 0230-kth-smallest-element-in-a-bst C solution (with recursive inordertraversal)

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.c
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.c
@@ -6,6 +6,26 @@
  *     struct TreeNode *right;
  * };
  */
+
+// inOrderTraversal helper function
+void inOrderTraversal(struct TreeNode* root, int* k, int* ans){
+    // base case
+    if ((*ans != -1) || root == NULL) return;
+    // recursively call inOrderTraversal function for the left-subTree
+    inOrderTraversal(root->left, k, ans);
+    // drecrement k as we visited each node
+    (*k)--;
+    // Check if this node is the k-th smallest
+    if (*k == 0){
+        *ans = root->val; // store the node value in ans
+        return; // return to stop further processing
+    }
+    // Continue to the right subtree if not found
+    inOrderTraversal(root->right, k, ans);
+}
+
 int kthSmallest(struct TreeNode* root, int k) {
-    
+    int ans = -1; // initialize answer as -1
+    inOrderTraversal(root, &k, &ans); // pass addresses of k and ans
+    return ans; // return the found value
 }
--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -6,13 +6,23 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), 
-left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
+    // inOrderTraversal helper function
+    void inOrderTraversal (TreeNode* root, vector<int> &v){
+        if (root == NULL) return;
+        inOrderTraversal(root->left, v);
+        v.push_back(root->val);
+        inOrderTraversal(root->right, v);
+    }
     int kthSmallest(TreeNode* root, int k) {
-        
+        // initialize a vector to store the inorder traversal of the BST
+        vector<int> v;
+        inOrderTraversal(root, v);
+        // because it's BST, so after inordertraversal will get a vector sorted in ascending order.
+        return v[k-1]; // vector's index start from 0, therefore k-1 to get Kth smallest value
     }
 };
