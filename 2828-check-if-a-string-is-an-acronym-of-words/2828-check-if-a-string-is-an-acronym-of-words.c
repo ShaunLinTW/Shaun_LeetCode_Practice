fc0bd75be364af380f4c26db693e1075645bc7a7
HsuanYouLin <yoyomy7198come@gmail.com>
2024-05-21 17:50:00 -0500

    update 2828-check-if-a-string-is-an-acronym-of-words C/C++ solutions

--- 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.c
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.c
@@ -1,3 +1,7 @@
 bool isAcronym(char ** words, int wordsSize, char * s){
-
+    // initialize a acronym string
+    char * str = (char *)malloc(sizeof(char) * 1000);
+    str[0] = '\0';
+    for(int i = 0; i < wordsSize; i++) strncat(str, &words[i][0], 1);
+    return (strcmp(str, s) == 0);
 }
--- 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.cpp
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isAcronym(vector<string>& words, string s) {
-        
+        // initialize a acronym string
+        string str = "";
+        for(auto w: words) str += w[0];
+        return (str == s);
     }
 };
