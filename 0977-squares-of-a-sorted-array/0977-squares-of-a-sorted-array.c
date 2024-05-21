b6e45f5b5dd1863639fcc4dd1214e12c46be66e1
HsuanYouLin <yoyomy7198come@gmail.com>
2024-05-21 18:11:15 -0500

    update 0977-squares-of-a-sorted-array C/C++ solutions

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.c
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.c
@@ -1,6 +1,34 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+int cmp(const void* a, const void* b){
+    return *((int*)a) - *((int*)b);
+}
+
 int* sortedSquares(int* nums, int numsSize, int* returnSize) {
-    
+    // simplest solution using quick sort
+    // for (int i = 0; i < numsSize; i++) nums[i] *= nums[i];
+    // qsort(nums, numsSize, sizeof(int), cmp);
+    // *returnSize = numsSize;
+    // return nums;
+
+    // two pointers solution (faster)
+    // initialize two pointers
+    int start = 0;
+    int end = numsSize - 1;
+    // initialize a result array
+    int* result = (int*)malloc(sizeof(int) *numsSize);
+
+    for (int i = numsSize - 1; i >= 0; i--){
+        if (abs(nums[start]) >= abs(nums[end])){
+            result[i] = nums[start] * nums[start];
+            start ++;
+        }
+        else{
+            result[i] = nums[end] * nums[end];
+            end --;
+        }
+    }
+    *returnSize = numsSize;
+    return result;
 }
--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        
+        // simplest solution using quick sort
+        // for (auto &x: nums) x *= x;
+        // sort(nums.begin(), nums.end());
+        // return nums;
+
+        // two pointers solution (faster)
+        int numsSize = nums.size();
+        int start = 0;
+        int end = numsSize - 1;
+        // initialize a result vector
+        vector<int> result(numsSize);
+
+        for (int i = numsSize - 1; i >= 0; i--){
+            if (abs(nums[start]) >= abs(nums[end])){
+                result[i] = nums[start] * nums[start];
+                start ++;
+            }
+            else{
+                result[i] = nums[end] * nums[end];
+                end --;
+            }
+        }
+        return result;
     }
 };
