21be29567d3182a9c45f3d8b0a454268551cebcc
HsuanYouLin <yoyomy7198come@gmail.com>
2024-05-21 17:40:06 -0500

    update 0367-valid-perfect-square C/C++ solutions

--- 0367-valid-perfect-square/0367-valid-perfect-square.c
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -1,3 +1,15 @@
 bool isPerfectSquare(int num) {
-    
+    /* binary search solution */
+    int start = 0; 
+    int end = num;
+
+    while(start <= end){
+        // update mid value
+        long long mid = start + (end - start) / 2;
+        
+        if(mid * mid == num) return true;
+        else if(mid * mid > num) end = mid - 1;
+        else start = mid + 1;
+    }
+    return false;
 }
--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        
+        /* binary search solution */
+        // initialize start and end value
+        int start = 0;
+        int end = num;
+
+        while(start <= end){
+            long long mid = start + (end - start) / 2;
+
+            if(mid * mid == num) return true;
+            else if(mid * mid > num) end = mid - 1; // update end value
+            else start = mid + 1; // update start value
+        }
+        return false;
     }
 };
-
