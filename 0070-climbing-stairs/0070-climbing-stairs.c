c281ac1122b274a4d32c07e35bd959476a3c9397
HsuanYouLin <yoyomy7198come@gmail.com>
2024-06-03 16:12:24 -0500

    update 0070-climbing-stairs C/C++ solutions

--- 0070-climbing-stairs/0070-climbing-stairs.c
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -1,3 +1,15 @@
 int climbStairs(int n) {
-    
+    if (n == 0 || n == 1) return 1; // edge case, 0 and 1 only have one way climb to the top
+    // recursive solution //
+    // return climbStairs(n - 1) + climbStairs(n - 2);
+
+    // space optimized solution //
+    int prev = 1, curr = 1;
+    for (int i = 2; i <= n; i++){
+        int temp = curr;
+        curr = prev + curr;
+        prev = temp;
+    }
+    return curr;
+
 }
--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        
+        // --------classic fibonacci math question---------- //
+        if (n == 0 || n == 1) return 1; // edge case, 0 and 1 only have one way climb to the top
+        // recursive solution //
+        // return climbStairs(n-1) + climbStairs(n-2);
+
+        // DP table solution //
+        // initialize dp table
+        // vector<int> dp(n+1);
+        // dp[0] = dp[1] = 1; // initialize 0 and 1 as one, because they only have one way to climb to the top
+        // for (int i = 2; i <= n; i++){
+        //     dp[i] = dp[i - 1] + dp[i - 2];
+        // }
+        // return dp[n];
+
+        // space optimized solution //
+        int prev = 1, curr = 1;
+        for (int i = 2; i <= n; i++){
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
+
     }
 };
