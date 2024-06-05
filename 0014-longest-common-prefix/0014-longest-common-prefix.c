76558ccf34ea1b42b7610ba3ad9fe50a91e13b01
HsuanYouLin <yoyomy7198come@gmail.com>
2024-06-05 10:05:34 -0500

    update 0014-longest-common-prefix C/C++ solutions

--- 0014-longest-common-prefix/0014-longest-common-prefix.c
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -1,3 +1,31 @@
+// cmp function for qsort
+int cmp(const void* a, const void* b){
+    return strcmp(*(char**)a, *(char**)b);
+}
+
 char* longestCommonPrefix(char** strs, int strsSize) {
+    // empty case
+    if (strsSize == 0) return "";
     
+    // sort the strings, so we can easily compare the first and last string
+    qsort(strs, strsSize, sizeof(char*), cmp);
+    
+    // get the first and last string
+    char* first = strs[0];
+    char* last = strs[strsSize - 1];
+    
+    // initialize the result 
+    char* result = (char*)malloc(sizeof(char) * 1000);
+    int resultSize = 0;
+    
+    // compare the first and last string
+    for (int i = 0; i < strlen(first); i++){
+        if (first[i] == last[i]) {
+            result[resultSize++] = first[i];
+        } else {
+            break;
+        }
+    }
+    result[resultSize] = '\0';
+    return result;
 }
--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        
+        // get the string's length
+        int n = strs.size();
+        // empty case
+        if (n == 0) return "";
+
+        // sort the strings, so we can easily compare the first and last string
+        sort(strs.begin(), strs.end());
+
+        // get the first and last string
+        string first = strs[0];
+        string last = strs[n - 1];
+
+        // initialize the result 
+        string result = "";
+
+        // compare the first and last string
+        for (int i = 0; i < first.size(); i++){
+            if (first[i] == last[i]) result += first[i];
+            else break;
+        }
+        return result;
     }
 };
