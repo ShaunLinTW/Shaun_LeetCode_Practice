ccd74ccc2f8bb6fecd1085918b938fb38ffadd91
HsuanYouLin <yoyomy7198come@gmail.com>
2024-06-24 16:07:56 -0500

    update 0028-find-the-index-of-the-first-occurrence-in-a-string C/C++ solutions

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
@@ -1,3 +1,25 @@
 int strStr(char* haystack, char* needle) {
-    
+    // empty case 
+    if(needle[0] == '\0') return 0;
+    // two pointers method, initialize pointer i, j and length of haystack and needle
+    int i = 0;
+    int j = 0;
+    int haystack_len = strlen(haystack);
+    int needle_len = strlen(needle);
+
+    while(i < haystack_len && j < needle_len){
+        if (haystack[i] == needle[j]){
+            // increase two pointer if the string element match
+            i++;
+            j++;
+            // if the whole pattern has been matched, then return the value by going needle size steps backwards from the current index in array
+            if (j == needle_len) return i - needle_len;
+        }
+        // when no match, start checking from the haystack index
+        else{
+            i = i - j + 1; // goes backwards till whatever length has been matched, which means only update 1st pointer
+            j = 0;// check from pattern's beginning, which means restart 2nd pointer
+        }
+    }
+    return -1;
 }
--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        
+        // empty case
+        if(needle == "") return 0;
+    
+        // two pointers method
+        int i = 0;
+        int j = 0;
+
+        while(i < haystack.size() && j < needle.size()){
+            if(haystack[i] == needle[j]){
+                // increase two pointer if the string element match
+                i++;
+                j++;
+                // if the whole pattern has been matched, then return the value by going needle size steps backwards from the current index in array
+                if(j == needle.size()) return i - needle.size();
+            }
+            // when no match, start checking from the haystack index
+            else{
+                i = i - j + 1; // goes backwards till whatever length has been matched, which means only update 1st pointer
+                j = 0; // check from pattern's beginning, which means restart 2nd pointer
+            }
+        }
+        return -1;
     }
 };
