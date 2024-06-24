e911ecc4459de75044a1d200ec81ab2fa653be86
HsuanYouLin <yoyomy7198come@gmail.com>
2024-06-24 16:28:29 -0500

    update 1426-counting-elements C/C++ solutions

--- 1426-counting-elements/1426-counting-elements.c
+++ 1426-counting-elements/1426-counting-elements.c
@@ -1,3 +1,15 @@
 int countElements(int* arr, int arrSize) {
-    
+    // using array to store the count of each element
+    int count = 0;
+    int* hash = (int*)calloc(2000, sizeof(int));
+    for (int i = 0; i < arrSize; i++) {
+        hash[arr[i]]++;
+    }
+    for (int i = 0; i < arrSize; i++) {
+        if (hash[arr[i] + 1] > 0) {
+            count++;
+        }
+    }
+    free(hash);
+    return count;
 }
--- 1426-counting-elements/1426-counting-elements.cpp
+++ 1426-counting-elements/1426-counting-elements.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     int countElements(vector<int>& arr) {
+        int count = 0;
         
+        // initialize a vector of 1001 elements with 0
+        vector<int> v(1001, 0);
+
+        // using vector to store the count of each element
+        for (int i = 0; i < arr.size(); i++) {
+            v[arr[i]]++;
+        }
+        // check if the next element exists
+        for (int i = 0; i < 1000; i++) {
+            if(v[i] && v[i + 1]) count += v[i]; // if the next element exists, add the count of the current element
+        }
+
+        // unordered set method (AKA hash set, but no need to use sortered set)
+        // unordered_set<int> hashSet(arr.begin(), arr.end());
+
+        // for (int i = 0; i < arr.size(); i++) {
+        //     if(hashSet.find(arr[i]+1) != hashSet.end()) count++;
+        // }
+        return count;
     }
 };
