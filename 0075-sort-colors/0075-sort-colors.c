2ba980a59bc9f6ce462793ca66ec1d4f7bab6b6b
HsuanYouLin <yoyomy7198come@gmail.com>
2024-07-01 15:39:02 -0500

    update 0075-sort-colors C/C++ solutions

--- 0075-sort-colors/0075-sort-colors.c
+++ 0075-sort-colors/0075-sort-colors.c
@@ -1,3 +1,13 @@
 void sortColors(int* nums, int numsSize) {
-    
+    int zeros = 0, ones = 0;
+    for (int num = 0; num < numsSize; num++){
+        if(nums[num] == 0) zeros++;
+        else if(nums[num] == 1) ones++;
+    }
+
+    for (int i = 0; i < zeros; ++i) nums[i] = 0;
+
+    for (int i = zeros; i < zeros + ones; ++i) nums[i] = 1;
+
+    for (int i = zeros + ones; i < numsSize; ++i) nums[i] = 2;
 }
