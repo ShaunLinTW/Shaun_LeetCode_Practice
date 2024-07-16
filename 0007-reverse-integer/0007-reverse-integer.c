be16a05ec2278bfa08e329c4d460d2b31f36ec11
HsuanYouLin <yoyomy7198come@gmail.com>
2024-07-16 12:00:57 -0500

    update 0007-reverse-integer C/C++ solutions

--- 0007-reverse-integer/0007-reverse-integer.c
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -1,3 +1,10 @@
 int reverse(int x){
+    int reversed_x = 0;
 
+    while (x){
+        if (reversed_x > INT_MAX/10 || reversed_x < INT_MIN/10) return 0;
+        reversed_x = reversed_x*10 + x%10;
+        x = x/10;
+    }
+    return reversed_x;
 }
